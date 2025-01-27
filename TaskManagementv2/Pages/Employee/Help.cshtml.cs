3f30f29a3b3af8b66b30b53aaf7df57d28354d7e
Pen-Gun <namsang715@gmail.com>
2025-01-27 20:22:02 +0545

    last v2

---
+++ TaskManagementv2/Pages/Employee/Help.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TaskManagementv2.Pages.Employee
+{
+    public class HelpModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
